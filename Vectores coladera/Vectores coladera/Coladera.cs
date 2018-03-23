6d7c60cbb948be81881ff348b72222807af18e6f
KarinaDell <36238027+KarinaDell@users.noreply.github.com>
2018-03-23 15:01:44 -0600

    Add files via upload

---
+++ Vectores coladera/Vectores coladera/Coladera.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectores_coladera
+{
+    class Coladera
+    {
+        private int[] vector;
+
+        public int[] Vector { get => vector; }
+
+        public Coladera()
+        {
+            vector = new int[1000];
+        }
+
+        public void llenar()
+        {
+            for(int i = 0; i <1000; i++)
+            {
+                vector[i] = i + 1;
+            }
+        }
+
+        public void eliminar()
+        {
+            for(int i = 1; i < 1000 - 1; i++)
+            {
+                int m = i + 1;
+                for(int j = m+m; j < 1000; j += m)
+                {
+
+                vector[j-1] = 0;
+
+                }
+            }
+        }
+    }
+}
---
+++ Vectores coladera/Vectores coladera/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vectores_coladera
+{
+    public partial class Form1 : Form
+    {
+        Coladera coladera = new Coladera();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            txtMostrar.Text = mostrar();
+        }
+
+        public string mostrar()
+        {
+            string mostrar = "";
+            coladera.llenar();
+            coladera.eliminar();
+            for(int i = 1; i <1000 - 1; i++)
+            {
+                if (coladera.Vector[i] != 0)
+                    mostrar += coladera.Vector[i] + ", ";
+            }
+            return mostrar;
+        }
+    }
+}
